ecd8e20a2ab43bc3e8eb12248aea980de1efde1d
David <davidsim1035@gmail.com>
2024-11-12 13:05:29 +0100

    Add json data for recipes

--- assets/js/components/cards.js
+++ assets/js/components/cards.js
@@ -3,51 +3,52 @@ export async function getCardData() {
     "https://gist.githubusercontent.com/baiello/0a974b9c1ec73d7d0ed7c8abc361fc8e/raw/e598efa6ef42d34cc8d7e35da5afab795941e53e/recipes.json"
   );
   const cardData = await reponse.json();
-  console.log(cardData);
 
   const recipesGrid = document.getElementById("recipes_grid");
-  const card = document.createElement("article");
-  recipesGrid.append(card);
-  card.innerHTML = `
+  for (let i = 0; i < cardData.length; i++) {
+    const recipe = cardData[i];
+    // Limiter la description à 100 caractères
+    const maxDescriptionLength = 250;
+    const shortDescription =
+      recipe.description.length > maxDescriptionLength
+        ? recipe.description.slice(0, maxDescriptionLength) + "..."
+        : recipe.description;
+    const card = document.createElement("article");
+    card.innerHTML = `
     <div class="recipes_img">
-        <img src="/assets/img/json_recipes/${cardData[0].image}" alt="Image de la recette"/>
+        <img src="/assets/img/json_recipes/${recipe.image}" alt="Image de la recette" loading="lazy"/>
     </div>
     <div class="recipes_content">
         <div class="recipes_title">
-          <h2>${cardData[0].name}</h2>
+          <h2>${recipe.name}</h2>
         </div>
         <div class="recipes_steps">
             <h3>Recette</h3>
             <p>
-                ${cardData[0].description}
+                ${shortDescription}
             </p>
         </div>
         <div class="recipes_ingredients">
             <h3>Ingrédients</h3>
-            <ul>
-                <li>
-                  <h4 class="ingredient_title">${cardData[0].ingredients[0].ingredient}</h4>
-                  <p class="ingredient_amount">${cardData[0].ingredients[0].quantity}</p>
-                </li>
-                <li>
-                  <h4 class="ingredient_title">Crème de coco</h4>
-                  <p class="ingredient_amount">4 cuillères</p>
-                </li>
-                <li>
-                  <h4 class="ingredient_title">Glaçons</h4>
-                  <p class="ingredient_amount">2</p>
-                </li>
-                <li>
-                  <h4 class="ingredient_title">Jus de citron</h4>
-                  <p class="ingredient_amount">2</p>
-                </li>
-                <li>
-                  <h4 class="ingredient_title">Sucre</h4>
-                  <p class="ingredient_amount">20 g</p>
-                </li>
-              </ul>
+            <ul class="ingredient_list">
+            </ul>
             </div>
         </div>
   `;
-  return card;
+
+    const ingredientContainer = card.querySelector(".ingredient_list");
+    for (let j = 0; j < recipe.ingredients.length; j++) {
+      const ingredient = recipe.ingredients[j];
+      const ingredientList = document.createElement("li");
+      ingredientList.innerHTML = ` 
+      <h4 class="ingredient_title">${ingredient.ingredient}</h4>
+      <div class="amount_unit_container">
+          <p class="ingredient_amount">${ingredient.quantity || ""}</p>
+          <p class="ingredient_unit">${ingredient.unit || ""}</p>
+      </div>
+      `;
+      ingredientContainer.appendChild(ingredientList);
+    }
+    recipesGrid.appendChild(card);
+  }
 }
